c718417bbc328067fb64a8223de8e52fe8e4804b
HRiDOX <rgrrf123@gmail.com>
2022-09-13 21:35:16 +0600

    User Models SetUp

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,7 @@
-from audioop import maxpp
+
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-
+from django.db.models.fields.related import  OneToOneField
 # Create your models here.
 class UserManager(BaseUserManager):
     #When create function inside class firstly need to pass self --Hridoy
@@ -79,4 +79,22 @@ class User(AbstractBaseUser):
 
 
 class UserProfile(models.Model):
-    user
+    #OnetoOne Means one use can have only one user profile
+    #IN ON_delete section it means when the user is deleted, then the user profile should get deleted
+    user = OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='users/profile_pictures',blank=True,null=True)
+    cover_photo = models.ImageField(upload_to='users/cover_photos', blank=True, null=True)
+    address_line_1 = models.CharField(max_length=50, blank=True, null=True)
+    address_line_2 = models.CharField(max_length=50, blank=True, null=True)
+    country = models.CharField(max_length=15, blank=True, null = True)
+    state = models.CharField(max_length=15,blank=True,null=True)
+    city = models.CharField(max_length=15,blank=True,null=True)
+    pin_code =  models.CharField(max_length=7,blank=True,null=True)
+    latitude =  models.CharField(max_length=20,blank=True,null=True)
+    longitude =  models.CharField(max_length=20,blank=True,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return self.user.email
+
