0fa22ad97120e65ad24e5cf6fdb082957188f408
HRiDOX <rgrrf123@gmail.com>
2022-10-11 00:18:33 +0600

    Vendor dashboard url setup slightly

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path,include
 from .import views
 
 urlpatterns = [
+    path('',views.myVendorAccount),
     path('registerUser/', views.registerUser, name='registerUser'),
     path('registerVendor/', views.registerVendor, name='registerVendor'),
 
@@ -9,7 +10,7 @@ urlpatterns = [
     path('login_vendor/', views.login_vendor, name='login_vendor'),
     path('logout/', views.logout, name='logout'),
     path('myUserAccount/',views.myUserAccount, name='myUserAccount'),
-     path('myVendorAccount/',views.myVendorAccount, name='myVendorAccount'),
+    path('myVendorAccount/',views.myVendorAccount, name='myVendorAccount'),
     path('custDashboard/', views.custDashboard, name='custDashboard'),
     path('vendorDashboard/', views.vendorDashboard, name='vendorDashboard'),
     path('activateUser/<uidb64>/<token>/', views.activateUser, name='activateUser'),
@@ -19,6 +20,8 @@ urlpatterns = [
     path('forgot_password/', views.forgot_password, name='forgot_password'),
     path('reset_password_validate/<uidb64>/<token>/', views.reset_password_validate, name='reset_password_validate'),
     path('reset_password/', views.reset_password, name='reset_password'),
+
+    path('vendor/', include('vendor.urls'))
 
 
 
