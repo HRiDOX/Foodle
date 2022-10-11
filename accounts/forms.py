c292c132c656715dcb43f3f433e86251bc526304
AnikaIslamIUT <anikaislam@iut-dhaka.edu>
2022-10-12 02:55:45 +0600

    Custom Validator Function Allow Only Images
    Created Custom Validator Function Allow Only Images and implemented on Restaurant Profile update form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import User, UserProfile
+from .validators import allow_only_images_validator
+
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -20,8 +22,8 @@ class UserForm(forms.ModelForm):
 
 class UserProfileForm(forms.ModelForm):
     #address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Start typing...', 'required': 'required'}))
-    profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}))#, validators=[allow_only_images_validator])
-    cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}))#, validators=[allow_only_images_validator])
+    profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+    cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
     
     # latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
     # longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
