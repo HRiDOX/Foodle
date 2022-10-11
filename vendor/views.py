fe35b218b3bc2c451998b59d86aaa64e74facab0
AnikaIslamIUT <anikaislam@iut-dhaka.edu>
2022-10-12 01:50:12 +0600

    More features added on Restaurant Profile form

--- vendor/views.py
+++ vendor/views.py
@@ -1,17 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .forms import VendorForm
 from accounts.forms import UserProfileForm
+
+from accounts.models import UserProfile
+from .models import Vendor
 
 
 # Create your views here.
 
 def vprofile(request):
-    profile_form = UserProfileForm()
-    vendor_form = VendorForm()
+    profile = get_object_or_404(UserProfile, user=request.user)
+    vendor = get_object_or_404(Vendor, user=request.user)
+
+    profile_form = UserProfileForm(instance=profile)
+    vendor_form = VendorForm(instance=vendor)
 
     context = {
         'profile_form': profile_form,
         'vendor_form' :  vendor_form,
+        'profile': profile,
+        'vendor': vendor,
     }
     return render(request,'vendor/vprofile.html', context)
 
