f5afd425f6e31b2415d5310ca05416e22cd4acb8
AnikaIslamIUT <anikaislam@iut-dhaka.edu>
2022-10-14 02:11:20 +0600

    Url Path Adjustment And Availability Badge

--- Foodle/urls.py
+++ Foodle/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('home_vendor/', views.home_vendor,name="home_vendor"),
     path('', views.home, name='home'),
-    path('accounts/',include('accounts.urls'))
+    path('',include('accounts.urls'))
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
