36bf8fcec28595dbc989b8889f4f7ee6bae61711
Zaarin Anjum <zaarinanjum@iut-dhaka.edu>
2022-10-14 21:39:02 +0600

    Marketplace frontend created

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
 def marketplace(request):
+    vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)
     return render(request, 'marketplace/listings.html')
