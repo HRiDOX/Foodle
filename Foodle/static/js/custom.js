179a65575f3bfebaffe19fd3a73324451d408bed
AnikaIslamIUT <anikaislam@iut-dhaka.edu>
2022-10-15 22:28:53 +0600

    Showing cart is empty when all the items in the cart deleted

--- Foodle/static/js/custom.js
+++ Foodle/static/js/custom.js
@@ -172,7 +172,8 @@ $(document).ready(function(){
                     $('#cart_counter').html(response.cart_counter['cart_count']);
                     swal(response.status, response.message, "success")
 
-                    removeCartItem(0, cart_id); 
+                    removeCartItem(0, cart_id);
+                    checkEmptyCart() 
                 } 
             }
         })
@@ -186,6 +187,16 @@ $(document).ready(function(){
             document.getElementById("cart-item-"+cart_id).remove()
         }
     
-}
+    }
+
+
+    // Check if the cart is empty
+    function checkEmptyCart(){
+        var cart_counter = document.getElementById('cart_counter').innerHTML
+        if(cart_counter == 0){
+            document.getElementById("empty-cart").style.display = "block";
+        }
+    }
+ 
 
 });    
