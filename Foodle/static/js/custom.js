4fb11a13ac36a8aa7fad18c7a4f76627b6f7c1ec
HRiDOX <rgrrf123@gmail.com>
2022-10-15 18:04:15 +0600

    decreasing cart items

--- Foodle/static/js/custom.js
+++ Foodle/static/js/custom.js
@@ -80,19 +80,22 @@ $(document).ready(function(){
         food_id = $(this).attr('data-id');
         url = $(this).attr('data-url');
 
-        data = {
-            food_id: food_id
-        }
-
+       
 
         $.ajax({
             type: 'GET',
             url: url,
-            data: data,
+           
             success: function(response){
                 console.log(response);
-                $('#cart_counter').html(response.cart_counter['cart_count'])
-                $('#qty-'+food_id).html(response.qty);
+                if (response.status == 'Failed') {
+                    console.log('Raise the error')
+                    
+                } else {
+                    $('#cart_counter').html(response.cart_counter['cart_count'])
+                    $('#qty-'+food_id).html(response.qty);
+                    
+                }
 
             }
         })    
@@ -106,6 +109,35 @@ $(document).ready(function(){
     
         $('#'+the_id).html(qty)
 
+    })
+
+    //decrease cart
+    $('.decrease_cart').on('click', function(e){
+        e.preventDefault();
+
+        food_id = $(this).attr('data-id');
+        url = $(this).attr('data-url');
+
+        
+
+
+        $.ajax({
+            type: 'GET',
+            url: url,
+            
+            success: function(response){
+                console.log(response)
+                if (response.status == 'Failed') {
+                    console.log(response)
+                } else {
+                    $('#cart_counter').html(response.cart_counter['cart_count'])
+                    $('#qty-'+food_id).html(response.qty);
+                    
+                }
+               
+
+            }
+        })    
     })
 
 });    
