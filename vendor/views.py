f7a0f992efef207580f9bc851b64fd832a3d04cc
HRiDOX <rgrrf123@gmail.com>
2022-10-29 19:49:07 +0600

    Opening Hours Form

--- vendor/views.py
+++ vendor/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import get_object_or_404,redirect, render
-from .forms import VendorForm
+from .forms import VendorForm,OpeningHourForm
 from accounts.forms import UserProfileForm
 
 from menu.forms import CategoryForm, FoodItemForm
 
 from accounts.models import UserProfile
-from .models import Vendor
+from .models import Vendor,OpeningHour
 from django.contrib import messages
 
 from django.contrib.auth.decorators import login_required, user_passes_test
@@ -198,7 +198,13 @@ def delete_food(request, pk=None):
 
           
 def opening_hours(request):
-    return render(request,'vendor/opening_hours.html')
+    opening_hours = OpeningHour.objects.filter(vendor=get_vendor(request))
+    form = OpeningHourForm()
+    context = {
+        'form':form,
+        'opening_hours':opening_hours
+    }
+    return render(request,'vendor/opening_hours.html',context)
         
 
 
