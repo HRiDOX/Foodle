df3187388024dd9515b208a987d115cfea75c2e7
Zaarin Anjum <zaarinanjum@iut-dhaka.edu>
2022-11-05 21:31:16 +0600

    Made customer profile cover section dynamic

--- Foodle/settings.py
+++ Foodle/settings.py
@@ -79,7 +79,8 @@ TEMPLATES = [
                 'accounts.context_processors.get_vendor',
                 'accounts.context_processors.get_google_api',
                 'marketplace.context_processors.get_cart_counter',
-                'marketplace.context_processors.get_cart_amounts'
+                'marketplace.context_processors.get_cart_amounts',
+                'accounts.context_processors.get_user_profile',
             ],
         },
     },
