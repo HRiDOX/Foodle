f146e0a05a1304254dafb36a16c17c35ec83eac9
Zaarin Anjum <zaarinanjum@iut-dhaka.edu>
2022-12-21 10:41:01 +0600

    Order Confirmation mail for users created

--- orders/views.py
+++ orders/views.py
@@ -6,8 +6,11 @@ from .models import Order, OrderedFood,Payment
 import simplejson as json
 from .utils import generate_order_number
 from django.http import HttpResponse, JsonResponse
+from accounts.utils import send_notification
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required(login_url='login')
 def place_order(request):
     cart_items = Cart.objects.filter(user=request.user).order_by('created_at')
     cart_count = cart_items.count()
@@ -60,7 +63,7 @@ def place_order(request):
 
     return render(request, 'orders/place_order.html')
 
-
+@login_required(login_url='login')
 def payments(request):
         # Check if the request is ajax or not
     if request.headers.get('x-requested-with') == 'XMLHttpRequest' and request.method == 'POST':
@@ -98,5 +101,17 @@ def payments(request):
             ordered_food.price = item.fooditem.price
             ordered_food.amount = item.fooditem.price * item.quantity # total amount
             ordered_food.save()
-       
-        return HttpResponse('saved ordered food')
+
+         # SEND ORDER CONFIRMATION EMAIL TO THE CUSTOMER
+        mail_subject = 'Thank you for ordering with us.'
+        mail_template = 'orders/order_confirmation_email.html'
+
+        context = {
+            'user': request.user,
+            'order': order,
+            'to_email': order.email,
+        }
+
+        send_notification(mail_subject, mail_template, context)
+
+        
