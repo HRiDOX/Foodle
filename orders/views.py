6685e905c3997add62136ecdace0ca4bd7a1d4d8
Zaarin Anjum <zaarinanjum@iut-dhaka.edu>
2022-12-21 21:01:06 +0600

    Order complete page setup and get request generated

--- orders/views.py
+++ orders/views.py
@@ -140,3 +140,7 @@ def payments(request):
         }
         return JsonResponse(response)
     return HttpResponse('Payments view') 
+
+
+def order_complete(request):
+    return render(request, 'orders/order_complete.html')
