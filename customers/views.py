0376f6a56ab1209fb9e49606550eb7d778a81334
AnikaIslamIUT <anikaislam@iut-dhaka.edu>
2022-12-23 03:27:37 +0600

    implemented customer dashboard

--- customers/views.py
+++ customers/views.py
@@ -4,6 +4,7 @@ from accounts.forms import UserInfoForm, UserProfileForm
 from accounts.models import UserProfile
 from django.contrib import messages
 from orders.models import Order, OrderedFood
+import simplejson as json
 
 
 @login_required(login_url='login')
@@ -34,7 +35,7 @@ def cprofile(request):
 
 
 def my_orders(request):
-    orders = Order.objects.filter(user=request.user).order_by('-created_at')
+    orders = Order.objects.filter(user=request.user, is_ordered=True).order_by('-created_at')
 
     context = {
         'orders': orders,
@@ -42,4 +43,20 @@ def my_orders(request):
     return render(request, 'customers/my_orders.html', context)
 
 def order_detail(request, order_number):
-    return render(request, 'customers/order_detail.html')
+    try:
+        order = Order.objects.get(order_number=order_number, is_ordered=True)
+        ordered_food = OrderedFood.objects.filter(order=order)
+        subtotal = 0
+        for item in ordered_food:
+            subtotal += (item.price * item.quantity)
+        tax_data = json.loads(order.tax_data)
+        context = {
+            'order': order,
+            'ordered_food': ordered_food,
+            'subtotal': subtotal,
+            'tax_data': tax_data,
+        }
+        return render(request, 'customers/order_detail.html', context)
+    except:
+        return redirect('customer')
+   
