dcf322c34709363cbeed6ef62b14a687c4ebd713
HRiDOX <rgrrf123@gmail.com>
2022-12-25 02:43:37 +0600

    calculating money for each vendor

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from marketplace.models import Cart
+from marketplace.models import Cart, Tax
 from marketplace.context_processors import get_cart_amounts
 from .forms import OrderForm
 from .models import Order, OrderedFood,Payment
@@ -8,6 +8,8 @@ from .utils import generate_order_number
 from django.http import HttpResponse, JsonResponse
 from accounts.utils import send_notification
 from django.contrib.auth.decorators import login_required
+from menu.models import FoodItem
+from django.contrib.sites.shortcuts import get_current_site
 # Create your views here.
 
 @login_required(login_url='login')
@@ -20,6 +22,35 @@ def place_order(request):
     for i in cart_items:
         if i.fooditem.vendor.id not in vendors_ids:
             vendors_ids.append(i.fooditem.vendor.id)
+    get_tax = Tax.objects.filter(is_active=True)
+    subtotal = 0
+    total_data = {}
+    #empty dictionary
+    k = {}
+    for i in cart_items:
+        fooditem = FoodItem.objects.get(pk=i.fooditem.id, vendor_id__in=vendors_ids)
+        v_id = fooditem.vendor.id
+        if v_id in k:
+            subtotal = k[v_id]
+            subtotal += (fooditem.price * i.quantity)
+            k[v_id] = subtotal
+        else:
+            subtotal = (fooditem.price * i.quantity)
+            k[v_id] = subtotal
+        
+
+        print(fooditem, fooditem.vendor.id,k)
+
+        #caculate the vat data
+        tax_dict = {}
+        for i in get_tax:
+            tax_type = i.tax_type
+            tax_percentage = i.tax_percentage
+            tax_amount = round((tax_percentage * subtotal)/100, 2)
+            tax_dict.update({tax_type: {str(tax_percentage) : str(tax_amount)}})
+        # Construct total data
+        total_data.update({fooditem.vendor.id: {str(subtotal): str(tax_dict)}})
+        print(total_data)
 
 
 
@@ -45,13 +76,13 @@ def place_order(request):
             order.user = request.user
             order.total = grand_total
             order.tax_data = json.dumps(tax_data)
-            #order.total_data = json.dumps(total_data)
+            order.total_data = json.dumps(total_data)
             order.total_tax = total_tax
             order.payment_method = request.POST['payment_method']
             order.save()
             order.order_number = generate_order_number(order.id)
             order.vendors.add(*vendors_ids)
-            #start bcz it'll recursivley add vendors_ids
+            #star bcz it'll recursivley add vendors_ids
             order.save()
 
             
